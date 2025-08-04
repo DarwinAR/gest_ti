32d7a1ff38cb1a2b383f30a3cfb2762ff84f675a
Darwin Arroyave <darwinarroyave14@gmail.com>
2025-08-04 15:20:49 -0500

    Se crea nueva pagina horario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { PInicial } from './p-inicial/p-inicial';
 import { Inicio } from './inicio/inicio';
+import { Horario } from './horario/horario';
 
 export const routes: Routes = [
     {path:'',component:Inicio},
-    {path:'p_inicial',component:PInicial}
+    {path:'p_inicial',component:PInicial},
+    {path:'horario',component:Horario}
 ];
