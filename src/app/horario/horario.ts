c5ca3bbeaa00cd9a43708b4d1105a83a77670eb0
Darwin Arroyave <darwinarroyave14@gmail.com>
2025-08-12 19:29:53 -0500

    Se trabaja en horario

--- src/app/horario/horario.ts
+++ src/app/horario/horario.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { routes } from '../app.routes';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-horario',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './horario.html',
   styleUrl: './horario.css'
 })
 export class Horario {
+  constructor(public ruta: Router){}
+  
+  semana = [
+    {dia:'Lunes'},
+    {dia:'Martes'},
+    {dia:'Miercoles'},
+    {dia:'Jueves'},
+    {dia:'Viernes'},
+    {dia:'Sabado'}
+  ]
+  volver(){
+    this.ruta.navigate(['p_inicial']);
 
+  }
 }
