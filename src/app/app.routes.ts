2e6383f49f027300268e5bd0470a9dc953e029e1
Darwin Arroyave <darwinarroyave14@gmail.com>
2025-08-25 16:27:47 -0500

    subo cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import path from 'path';
 import { PInicial } from './p-inicial/p-inicial';
 import { Inicio } from './inicio/inicio';
 import { Horario } from './horario/horario';
+import { FormReserva } from './form-reserva/form-reserva';
+
 
 export const routes: Routes = [
     {path:'',component:Inicio},
     {path:'p_inicial',component:PInicial},
-    {path:'horario',component:Horario}
+    {path:'horario',component:Horario},
+    {path:'FormReserva',component:FormReserva}
 ];
